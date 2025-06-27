25285320f25f7c8f798745f0b736955cd80c5116
BodenKellyCrow <bossalando177@gmail.com>
2025-06-27 23:11:45 +0800

    Update wsgi.py

--- doomscrollr/wsgi.py
+++ doomscrollr/wsgi.py
@@ -1,1 +1,9 @@
+# doomscrollr/wsgi.py
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doomscrollr.settings')
+
+application = get_wsgi_application()
 # Django wsgi.py placeholder
