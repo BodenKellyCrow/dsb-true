c03a64f434c3c005036a578c2b58683c995fbe18
BodenKellyCrow <bossalando177@gmail.com>
2025-06-27 23:43:22 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
+import os
+import sys
 
-print('Django manage.py placeholder')
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doomscrollr.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Is it installed and available on your PYTHONPATH?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
