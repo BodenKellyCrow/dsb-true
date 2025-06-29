a69d356243da3288e37011933cd7aeee93e01529
BodenKellyCrow <bossalando177@gmail.com>
2025-06-30 03:17:34 +0800

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_overview, name="api-overview"),
+    path('projects/', views.project_list, name='project_list'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+    path('projects/<int:pk>/fund/', views.fund_project, name='fund_project'),
 ]
