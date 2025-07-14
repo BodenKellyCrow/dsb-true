28a1c406e8909b442bf22defd616bfaf281d63de
BodenKellyCrow <bossalando177@gmail.com>
2025-07-14 14:31:02 +0000

    serializers changed

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,13 +1,28 @@
 from rest_framework import serializers
-from .models import Project, Transaction
+from django.contrib.auth.models import User
+from .models import Project, Transaction, UserProfile
+
 
 class ProjectSerializer(serializers.ModelSerializer):
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+
     class Meta:
         model = Project
         fields = '__all__'
 
+
 class TransactionSerializer(serializers.ModelSerializer):
+    sender_username = serializers.CharField(source='sender.username', read_only=True)
+    receiver_username = serializers.CharField(source='receiver.username', read_only=True)
+
     class Meta:
         model = Transaction
         fields = '__all__'
-        read_only_fields = ['sender', 'recipient', 'timestamp']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
