b98ac20b3c2ee14c2968c0599791cf3861b4eef5
BodenKellyCrow <bossalando177@gmail.com>
2025-08-27 22:41:37 +0000

    pleeee

--- doomscrollr/settings.py
+++ doomscrollr/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+import dj_database_url 
+from dotenv import load_dotenv
 
 # Build paths
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,11 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'doomscrollr.wsgi.application'
 
 # Database
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Use DATABASE_URL from environment variables
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
